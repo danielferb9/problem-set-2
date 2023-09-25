8d2507fe8c8fac4c72cf4aa49807874d281231b5
danielferb9 <d.rianob@uniandes.edu.co>
2023-09-25 17:19:24 -0500

    Update code.r

--- code/code.r
+++ code/code.r
@@ -1,1 +1,7 @@
-Hola
+## Daniel Felipe Riaño Barrera - 202112753
+## Andrés Felipe Guevara Pineda - 202022636
+R.version
+require("pacman")
+require("rio")
+require("data.table")
+require("tidyverse")
