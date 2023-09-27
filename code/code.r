8f6f250190915cfb6025d7a37535584012060597
juankamilon <j.narvaezo@uniandes.edu.co>
2023-09-26 23:01:27 -0500

    Cambios Juan Kamilo 1
    Primero, cree las carpetas que nos piden junto con las bases de datos. Segundo, copié el código que tenía hasta el momento

--- code/code.r
+++ code/code.r
@@ -1,7 +1,38 @@
 ## Daniel Felipe Riaño Barrera - 202112753
 ## Andrés Felipe Guevara Pineda - 202022636
-R.version
-require("pacman")
-require("rio")
-require("data.table")
-require("tidyverse")
+#Juan Kamilo Narvaez - 202113808
+R.version.string
+
+#Limpiar el entorno
+rm(list=ls()) 
+
+## instalar/llamar pacman
+install.packages("pacman")
+library(pacman)
+require(pacman)
+
+## usar la función p_load de pacman para instalar/llamar las librerías de la clase
+p_load(rio,
+       skimr,
+       janitor,
+       data.table,
+       tidyverse)
+#Verificar el directorio
+getwd()
+list.files()
+
+## 1.importar/exportar
+#importar bases de datos
+location <-       import(file= "input/Modulo de sitio o ubicacion.csv", encoding="UTF-8") 
+identification <- import(file= "input/Modulo de identificacion.csv", encoding="UTF-8") 
+
+#Exportar bases de datos
+export(x=location , file="output/location.rds")
+export(x=identification , file="output/identification.rds")
+
+##3. Generar variables
+#tipo de empresa
+identification = mutate(identification, bussiness_type = case_when(GRUPOS4 == 1 ~ "agricultura",
+                                                                   GRUPOS4 == 2 ~ "industria manufacturera",
+                                                                   GRUPOS4 == 3 ~ "comercio",
+                                                                   GRUPOS4 == 4 ~ "servicos"))
